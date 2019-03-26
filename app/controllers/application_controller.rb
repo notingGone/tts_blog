70a75ff5204a28999310495bce69fdea00e5b7d8
Elliott C. Woodward <elliottcwoodward@gmail.com>
2019-03-26 16:13:00 -0400

    Set timezone to Eastern app-wide. Left comment in application_controller for setting timestamps to display in user's local timezone.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  #https://stackoverflow.com/questions/4269270/rails-3-setting-the-timezone-to-the-current-users-timezone
   # before_action :set_user_time_zone
 
   # private
 
   # def set_user_time_zone
-  #   Time.zone = current_user.time_zone if logged_in?
+  #   Time.zone = current_user.settings(:main).time_zone if current_user
+  #   yield
   # end
 
   protected
