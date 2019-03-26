9a523d75a3e7179d54804571dc86e56e603f6336
Elliott C. Woodward <elliottcwoodward@gmail.com>
2019-03-26 13:33:40 -0400

    Make blog index page griddy. Allow for user.avatar.nil?

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,15 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :validatable
 
   mount_uploader :avatar, AvatarUploader
+
+  after_initialize :init
+
+  def init
+    self.avatar ||= "default-avatar.jpg"
+  end
 
   has_many :posts
   has_many :comments
